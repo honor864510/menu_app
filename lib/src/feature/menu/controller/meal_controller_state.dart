605e27481c56aaad3dced2fc10cf22ed985d85aa
Honor <honor864510@gmail.com>
2025-10-17 05:26:50 +0900

    add controller state

---
+++ lib/src/feature/menu/controller/meal_controller_state.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/foundation.dart';
+import 'package:menu_app/src/feature/menu/model/meal_category_entity.dart';
+import 'package:menu_app/src/feature/menu/model/meal_entity.dart';
+
+@immutable
+final class MealControllerStateEntity {
+  const MealControllerStateEntity({
+    required this.meals,
+    required this.categories,
+    required this.mealsCount,
+    required this.selectedMeal,
+    required this.selectedCategory,
+  });
+
+  final List<MealEntity> meals;
+  final List<MealEntity> categories;
+  final int mealsCount;
+  final MealEntity selectedMeal;
+  final MealCategoryEntity selectedCategory;
+
+  MealControllerStateEntity copyWith({
+    List<MealEntity>? meals,
+    List<MealEntity>? categories,
+    int? mealsCount,
+    MealEntity? selectedMeal,
+    MealCategoryEntity? selectedCategory,
+  }) => MealControllerStateEntity(
+    meals: meals ?? this.meals,
+    categories: categories ?? this.categories,
+    mealsCount: mealsCount ?? this.mealsCount,
+    selectedMeal: selectedMeal ?? this.selectedMeal,
+    selectedCategory: selectedCategory ?? this.selectedCategory,
+  );
+
+  // Optional: Add equality and toString for better debugging
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is MealControllerStateEntity &&
+        listEquals(other.meals, meals) &&
+        listEquals(other.categories, categories) &&
+        other.mealsCount == mealsCount &&
+        other.selectedMeal == selectedMeal &&
+        other.selectedCategory == selectedCategory;
+  }
+
+  @override
+  int get hashCode =>
+      meals.hashCode ^ categories.hashCode ^ mealsCount.hashCode ^ selectedMeal.hashCode ^ selectedCategory.hashCode;
+}
+
+/// {@template meal_controller_state}
+/// MealControllerState.
+/// {@endtemplate}
+sealed class MealControllerState extends _$MealControllerStateBase {
+  /// {@macro meal_controller_state}
+  const MealControllerState({required super.data, required super.message});
+
+  /// Idle
+  /// {@macro meal_controller_state}
+  const factory MealControllerState.idle({required MealControllerStateEntity data, String message}) =
+      MealControllerState$Idle;
+
+  /// Processing
+  /// {@macro meal_controller_state}
+  const factory MealControllerState.processing({required MealControllerStateEntity data, String message}) =
+      MealControllerState$Processing;
+
+  /// Failed
+  /// {@macro meal_controller_state}
+  const factory MealControllerState.failed({required MealControllerStateEntity data, String message}) =
+      MealControllerState$Failed;
+
+  /// Initial
+  /// {@macro meal_controller_state}
+  factory MealControllerState.initial({required MealControllerStateEntity data, String? message}) =>
+      MealControllerState$Idle(data: data, message: message ?? 'Initial');
+}
+
+/// Idle
+final class MealControllerState$Idle extends MealControllerState {
+  const MealControllerState$Idle({required super.data, super.message = 'Idle'});
+
+  @override
+  String get type => 'idle';
+}
+
+/// Processing
+final class MealControllerState$Processing extends MealControllerState {
+  const MealControllerState$Processing({required super.data, super.message = 'Processing'});
+
+  @override
+  String get type => 'processing';
+}
+
+/// Failed
+final class MealControllerState$Failed extends MealControllerState {
+  const MealControllerState$Failed({required super.data, super.message = 'Failed'});
+
+  @override
+  String get type => 'failed';
+}
+
+@immutable
+abstract base class _$MealControllerStateBase {
+  const _$MealControllerStateBase({required this.data, required this.message});
+
+  /// Type alias for [MealControllerState].
+  abstract final String type;
+
+  /// Data entity payload.
+  @nonVirtual
+  final MealControllerStateEntity data;
+
+  /// Message or description.
+  @nonVirtual
+  final String message;
+
+  /// Check if is Idle.
+  bool get isIdle => this is MealControllerState$Idle;
+
+  /// Check if is Processing.
+  bool get isProcessing => this is MealControllerState$Processing;
+
+  /// Check if is Failed.
+  bool get isFailed => this is MealControllerState$Failed;
+}
